2d755f9b408980ff0d106ead47697fcd6017c3a1
Jey Val <jeremv@gmail.com>
2020-05-26 20:20:24 +0200

    mistercocktail routes update

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, presence: true
+  validates_associated :cocktail, presence: true
+  validates_associated :ingredient, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
